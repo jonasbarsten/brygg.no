8f974cd4eb61c3d228d22fa35a52b1bdac4943d6
Jonas Barsten <jonasbarsten@gmail.com>
2018-10-31 22:18:19 +0100

    Changed opening hours

--- imports/ui/navigation/FrontFooter.js
+++ imports/ui/navigation/FrontFooter.js
@@ -59,11 +59,13 @@ class FrontFooter extends Component {
 						<span>ÅPNINGSTIDER:</span>
 						<br />
 						<div style={{fontFamily: 'Plaak4Terme-34-RegularA'}}>
-							<span>MANDAG - FREDAG 10:00 TIL 03:00</span>
+							<span>MANDAG - TIRSDAG 08:00 TIL 01:00</span>
+							<br />
+							<span>ONSDAG - FREDAG 08:00 TIL 03:00</span>
 							<br />
 							<span>LØRDAG 11:00 TIL 03:00</span>
 							<br />
-							<span>SØNDAG 12:00 TIL 03:00</span>
+							<span>SØNDAG 15:00 TIL 23:00</span>
 							<br />
 							<br />
 							<span>TAPPEBAR ÅPNER KL 14:00 på hverdager og samtidig som åpning lørdag og søndag</span>
